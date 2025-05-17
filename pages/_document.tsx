ce842ce5c32bf4ac4f3e1b7bbdf2842ba4d75f34
aicoder88 <reachdrago@gmail.com>
2025-05-16 23:34:01 -0400

    Fix deployment issues by removing Vite configs and updating Next.js setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,10 +30,32 @@ export default function Document() {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Purrify" />
         <meta name="mobile-web-app-capable" content="yes" />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T8WZ5D7R');`
+          }}
+        />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8WZ5D7R"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
+        {/* Chat plugin */}
+        <script src="https://app.simplebotinstall.com/js/chat_plugin.js" data-bot-id="40892" />
       </body>
     </Html>
   );
