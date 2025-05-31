d39420644558901ae4a6a3cedd8fe96df0ee2c88
chmod000chmod <10245994+chmod000chmod@users.noreply.github.com>
2025-05-30 23:44:46 -0400

    changing translation provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const canonicalUrl = '/';
   
   return (
-    <TranslationProvider language={locale}>
+    <TranslationProvider language={locale ?? 'en'}>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
