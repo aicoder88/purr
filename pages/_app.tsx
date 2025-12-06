279ec0f8fd468c13bc9a5d9f7961c250643b3b98
aicoder88 <reachdrago@gmail.com>
2025-12-06 20:35:38 +0100

    change font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import { Inter, Outfit } from 'next/font/google';
+import { Quicksand, Nunito, Inter, Outfit } from 'next/font/google';
 import Head from 'next/head';
 import { useEffect, useMemo } from 'react';
 import { DefaultSeo } from 'next-seo';
@@ -39,6 +39,8 @@ interface PageProps {
   [key: string]: unknown;
 }
 
+const quicksand = Quicksand({ subsets: ['latin'], display: 'swap', variable: '--font-quicksand', weight: ['300', '400', '500', '600', '700'] });
+const nunito = Nunito({ subsets: ['latin'], display: 'swap', variable: '--font-nunito', weight: ['200', '300', '400', '500', '600', '700', '800', '900'] });
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter', weight: ['400', '500', '700'] });
 const outfit = Outfit({ subsets: ['latin'], display: 'swap', variable: '--font-outfit', weight: ['400', '500', '700', '900'] });
 
@@ -172,7 +174,7 @@ function MyApp({ Component, pageProps }: AppProps<PageProps>) {
               }}
             />
 
-            <div className={`${inter.variable} ${outfit.variable}`}>
+            <div className={`${quicksand.variable} ${nunito.variable} ${inter.variable} ${outfit.variable}`}>
               <Layout>
                 <Component {...pageProps} />
               </Layout>
